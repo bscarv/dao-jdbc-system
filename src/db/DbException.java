2efd3f828b67d92faec2a5bb420bce3fb7d39de0
Bruno Silva <bs.carv@gmail.com>
2020-06-17 21:49:33 -0300

    Criadas classes Dao

--- src/db/DbException.java
+++ src/db/DbException.java
@@ -1,4 +1,4 @@
-package model.db;
+package db;
 
 public class DbException extends RuntimeException{
 
--- src/db/DbIntegrityException.java
+++ src/db/DbIntegrityException.java
@@ -1,4 +1,4 @@
-package model.db;
+package db;
 
 public class DbIntegrityException extends RuntimeException{
 
